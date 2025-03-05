0dee94b84e8098af33472b638fbf173798ea09ab
heiro <dev.heiro@gmail.com>
2025-03-05 15:56:01 +0900

    feat : 세션인증구현완료

--- src/main/java/org/codenova/talkhub/controller/HomeServlet.java
+++ src/main/java/org/codenova/talkhub/controller/HomeServlet.java
@@ -5,6 +5,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 
@@ -14,12 +15,34 @@ public class HomeServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        if(Math.random()>0.5) {
-            req.setAttribute("authentication", true);
-        }else {
-            req.setAttribute("authentication", false);
-        }
+    /*
+        HttpSession session = req.getSession();
+        String sid = session.getId();
+        boolean f = session.isNew();
+        long ct = session.getCreationTime();
+        long lat = session.getLastAccessedTime();
+        long now = System.currentTimeMillis();
 
+        System.out.println("session info =========================================");
+        System.out.println("sessionId : " + sid);
+        System.out.println("isNew = "+ f);
+        System.out.println("creationTime = "+ ct);
+        System.out.println("now - creationTime = "+ (now-ct));
+        System.out.println("now - lat = "+ (now - lat));
+
+        // session 저장 : setAttribute(String key, Object value)
+        // session 값확인 : getAttribute(String key)
+        // session 값삭제 : removeAttribute(String key)
+
+*/
+        HttpSession session = req.getSession();
+        boolean authentication;
+        if (session.getAttribute("user") != null) {
+            authentication = true;
+        } else {
+            authentication = false;
+        }
+        req.setAttribute("authentication", authentication);
         req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
     }
 }
---
+++ src/main/java/org/codenova/talkhub/controller/user/LogoutProceedServlet.java
@@ -0,0 +1,25 @@
+package org.codenova.talkhub.controller.user;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet("/user/logout-proceed")
+public class LogoutProceedServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+
+        // session.removeAttribute("user");
+
+        session.invalidate();
+
+
+        resp.sendRedirect(req.getContextPath()+"/index");
+    }
+}
